cafb85a60cde5e6175b7f11b66b28f791e6d7487
Antares94 <e.stradella94@outlook.com>
2020-07-23 16:10:40 -0300

    Rename 'routes' folder to 'chatbots' and add enpoints it

--- ws/app.py
+++ ws/app.py
@@ -12,25 +12,31 @@ from time import gmtime, strftime
 from datetime import date, datetime
 from importlib import reload
 
-import MySQLdb as db
+import MySQLdb
 from waitress import serve
 from flask import Flask, request
-from werkzeug.serving import run_simple
+from werkzeug import serving, wrappers
+
 
 from classes.database import Database
+from classes.settings import Settings
 from classes.utils import Utils
-from routes.facebook import facebook_blueprint
-from routes.instagram import instagram_blueprint
-from routes.whatsapp import whatsapp_blueprint
+from chatbots import chatbots_blueprint
+from chatbots.facebook import facebook_blueprint
+from chatbots.instagram import instagram_blueprint
+from chatbots.web import web_blueprint
+from chatbots.whatsapp import whatsapp_blueprint
 
 
 __appHostname = '0.0.0.0'
 __appPort = 5001
 app = Flask(__name__)
 
-app.register_blueprint(facebook_blueprint, url_prefix='/facebook')
-app.register_blueprint(instagram_blueprint, url_prefix='/instagram')
-app.register_blueprint(whatsapp_blueprint, url_prefix='/whatsapp')
+app.register_blueprint(chatbots_blueprint, url_prefix='/chatbots')
+app.register_blueprint(facebook_blueprint, url_prefix='/chatbots/fb')
+app.register_blueprint(instagram_blueprint, url_prefix='/chatbots/ig')
+app.register_blueprint(web_blueprint, url_prefix='/chatbots/wb')
+app.register_blueprint(whatsapp_blueprint, url_prefix='/chatbots/wa')
 
 
 if __name__ == '__main__':
@@ -43,6 +49,6 @@ if __name__ == '__main__':
         # serving.run_simple(__appHostname, __appPort, app, threaded=True, ssl_context=context)
 
 		# DESARROLLO
-		run_simple(__appHostname, __appPort, app)
+		serving.run_simple(__appHostname, __appPort, app)
 	except:
 		Utils.setLog()
